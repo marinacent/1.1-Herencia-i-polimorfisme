c84ed8c473ac0912c7ec52e2b4b9d7ebb53c208f
marinacent <52744370+marinacent@users.noreply.github.com>
2025-06-30 13:57:01 +0200

    call all methods from main

---
+++ smartphone/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Smartphone myPhone = new Smartphone("Pixel", "7a");
+        myPhone.call("678912345");
+        myPhone.takePicture();
+        myPhone.alarm();
+    }
+}
