910c754cdfabe25f91fe18703bc9b0edf0b11ebd
marinacent <52744370+marinacent@users.noreply.github.com>
2025-06-30 13:17:24 +0200

    create car class

---
+++ car/src/Car.java
@@ -0,0 +1,11 @@
+public class Car {
+    private static final String brand;
+    private static String model;
+    private final int power;
+
+    public Car(String brand, String model, int power) {
+        this.brand = brand;
+        this.model = model;
+        this.power = power;
+    }
+}
