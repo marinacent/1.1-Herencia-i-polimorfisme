bb666b631e1c2c2b052da267aef8d3dd749d56a2
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-01 10:17:07 +0200

    create writer class

---
+++ news/src/Writer.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Writer {
+    // nom, dni i sou
+    private String name;
+    private final int dni;
+    private static int salary = 1500;
+    private ArrayList<NewsReport> writerNews = new ArrayList<>;
+}
