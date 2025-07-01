3d606605e03ad0bb7701be236f2a6f371193b828
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-01 11:20:15 +0200

    create input reading menu

--- news/src/Main.java
+++ news/src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        int option;
+
+        do {
+            System.out.println("Pick an option:");
+            System.out.println("1  Add writer");
+            System.out.println("8  Exit");
+
+            while (!input.hasNextInt()) {
+                System.out.println("Please type a number:");
+                input.next();
+            }
+
+            option = input.nextInt();
+
+        } while (option != 8);
     }
 }
