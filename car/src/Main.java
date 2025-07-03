a1453a955837e1457a214024a0a03e9ece1318ad
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-03 10:59:28 +0200

    change unnecessary prints to comments

--- car/src/Main.java
+++ car/src/Main.java
@@ -5,11 +5,11 @@ public class Main {
         Car yourCar = new Car(4000);
         Car.setModel("A3");
 
-//        System.out.println("Final --> unique for each instance");
+//        Final --> unique for each instance
         System.out.println("My car: " + myCar.getPower());
         System.out.println("Your car: " + yourCar.getPower());
 
-//        System.out.println("\nStatic --> shared by all instances");
+//        Static --> shared by all instances
         System.out.println("All cars: " + Car.getModel());
         Car.setModel("A5");
         System.out.println("All cars: " + Car.getModel());
